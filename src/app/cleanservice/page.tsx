79e6766733011f5086f8cfb1f2a584cc7f94b2bd
Picnick0176 <nickmay.tv@gmail.com>
2025-03-14 10:25:36 +0700

    edit error useState

--- src/app/cleanservice/page.tsx
+++ src/app/cleanservice/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ImageSliderthree from "../components/ImageSliderthree";
+import ImageSliderthree from "/components/ImageSliderthree";
 
 import React, { useState } from "react";
 import Image from "next/image";
